df19c2188d930def728ec88e62fcdc5827d289dc
a402-01 <1>
2019-01-08 18:33:03 +0900

    file upload

--- app/src/main/java/com/example/tje/food/InfoReviewListHoler.java
+++ app/src/main/java/com/example/tje/food/InfoReviewListHoler.java
@@ -1,21 +1,48 @@
 package com.example.tje.food;
 
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.example.tje.food.R;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class InfoReviewListHoler extends RecyclerView.ViewHolder {
 
+    private static final String LOG_TAG = "inforeviewlistholder";
+    private static final String URL_MAPPING = "http://192.168.0.3:8080/Final/m";
+
     //2. 리스트 아이템에 있는 사용할 위젯 정의
-    TextView nicknameTv, likeCountTv, writeDateTv, contentsTv, reviewId;
-    ImageButton memberImageBtn;
+    TextView nicknameTv, likeCountTv, badCountTv, writeDateTv, contentsTv, reviewId;
+    ToggleButton like_btn, bad_btn;
     RatingBar ratingBar;
+
+    final Handler likeHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            likeCountTv.setText(msg.arg1 + "");
+        }
+    };
+    final Handler badHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            badCountTv.setText(msg.arg1 + "");
+        }
+    };
 
     //1. 생성자
     public InfoReviewListHoler(View v){
@@ -24,17 +51,274 @@ public class InfoReviewListHoler extends RecyclerView.ViewHolder {
         //3. findViewById로 연결
         nicknameTv = (TextView)v.findViewById(R.id.nicknameTv);
         likeCountTv = (TextView)v.findViewById(R.id.likeCountTv);
+        badCountTv = (TextView) v.findViewById(R.id.badCountTv);
         writeDateTv = (TextView)v.findViewById(R.id.writeDateTv);
         contentsTv = (TextView)v.findViewById(R.id.contentsTv);
         ratingBar = (RatingBar)v.findViewById(R.id.ratingBar);
-        memberImageBtn = (ImageButton)v.findViewById(R.id.memberImageBtn);
+        like_btn = (ToggleButton)v.findViewById(R.id.like_btn);
+        bad_btn = (ToggleButton)v.findViewById(R.id.bad_btn);
 
         reviewId = (TextView)v.findViewById(R.id.reviewId);
 
-        memberImageBtn.setOnClickListener(new View.OnClickListener() {
+        // like
+        like_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext().getApplicationContext(), "버튼 클릭" + reviewId.getText().toString(), Toast.LENGTH_SHORT).show();
+                AsyncTask.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d(LOG_TAG, "like btn click");
+
+                        if(like_btn.isChecked()) {      // toggle btn on
+
+                            if(bad_btn.isChecked()) {
+                                bad_btn.setChecked(false);
+                                //
+                                try {
+                                    URL endPoint = new URL(URL_MAPPING + "/cancelBadCount");
+                                    HttpURLConnection myConnection = (HttpURLConnection) endPoint.openConnection();
+                                    myConnection.setRequestMethod("POST");
+
+                                    int simple_review_id = Integer.parseInt(reviewId.getText().toString());
+
+                                    String requestParam = String.format("simple_review_id=%d", simple_review_id);
+                                    myConnection.getOutputStream().write(requestParam.getBytes());
+
+                                    if( myConnection.getResponseCode() == 200 ) {
+                                        Log.d(LOG_TAG, "cancel bad count" + myConnection.getResponseCode() + "");
+
+                                        BufferedReader in = new BufferedReader(
+                                                new InputStreamReader(myConnection.getInputStream()));
+                                        StringBuffer buffer = new StringBuffer();
+                                        String temp = null;
+
+                                        while((temp = in.readLine()) != null) {
+                                            buffer.append(temp);
+                                        }
+
+                                        Log.d(LOG_TAG, buffer.toString() + "");
+
+                                        Message count = badHandler.obtainMessage();
+                                        count.arg1 = Integer.parseInt(buffer.toString());
+                                        badHandler.sendMessage(count);
+                                    }
+
+                                } catch (Exception e) {
+                                    Log.d(LOG_TAG, "에러 발생");
+                                    e.getMessage();
+                                }
+                            }
+
+
+
+                            try {
+                                Log.d(LOG_TAG, "1");
+                                URL endPoint = new URL(URL_MAPPING + "/addLikeCount");
+                                HttpURLConnection myConnection = (HttpURLConnection) endPoint.openConnection();
+                                myConnection.setRequestMethod("POST");
+                                Log.d(LOG_TAG, "2");
+
+                                int simple_review_id = Integer.parseInt(reviewId.getText().toString());
+
+                                String requestParam = String.format("simple_review_id=%d", simple_review_id);
+                                myConnection.getOutputStream().write(requestParam.getBytes());
+                                Log.d(LOG_TAG, "3");
+
+                                if( myConnection.getResponseCode() == 200 ) {
+                                    Log.d(LOG_TAG, "add like count" + myConnection.getResponseCode() + "");
+
+                                    BufferedReader in = new BufferedReader(
+                                            new InputStreamReader(myConnection.getInputStream()));
+                                    StringBuffer buffer = new StringBuffer();
+                                    String temp = null;
+
+                                    while((temp = in.readLine()) != null) {
+                                        buffer.append(temp);
+                                    }
+
+                                    Log.d(LOG_TAG, buffer.toString() + "");
+
+                                    Message count = likeHandler.obtainMessage();
+                                    count.arg1 = Integer.parseInt(buffer.toString());
+                                    likeHandler.sendMessage(count);
+                                }
+
+                            } catch (Exception e) {
+                                Log.d(LOG_TAG, "에러 발생" + e.getMessage());
+                                e.getMessage();
+                            }
+
+                        } else {
+                            Log.d(LOG_TAG, "like btn cancel");
+
+                            try {
+                                URL endPoint = new URL(URL_MAPPING + "/cancelLikeCount");
+                                HttpURLConnection myConnection = (HttpURLConnection) endPoint.openConnection();
+                                myConnection.setRequestMethod("POST");
+
+                                int simple_review_id = Integer.parseInt(reviewId.getText().toString());
+
+                                String requestParam = String.format("simple_review_id=%d", simple_review_id);
+                                myConnection.getOutputStream().write(requestParam.getBytes());
+
+                                if( myConnection.getResponseCode() == 200 ) {
+                                    Log.d(LOG_TAG, "cancel like count" + myConnection.getResponseCode() + "");
+
+                                    BufferedReader in = new BufferedReader(
+                                            new InputStreamReader(myConnection.getInputStream()));
+                                    StringBuffer buffer = new StringBuffer();
+                                    String temp = null;
+
+                                    while((temp = in.readLine()) != null) {
+                                        buffer.append(temp);
+                                    }
+
+                                    Log.d(LOG_TAG, buffer.toString() + "");
+
+                                    Message count = likeHandler.obtainMessage();
+                                    count.arg1 = Integer.parseInt(buffer.toString());
+                                    likeHandler.sendMessage(count);
+                                }
+
+                            } catch (Exception e) {
+                                Log.d(LOG_TAG, "에러 발생");
+                                e.getMessage();
+                            }
+                        }
+
+                    }
+                });
+            }
+        });
+
+        // bad
+        bad_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AsyncTask.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d(LOG_TAG, "bad btn click");
+
+                        if(bad_btn.isChecked()) {
+
+                            if(like_btn.isChecked()) {
+                                like_btn.setChecked(false);
+                                //
+                                try {
+                                    URL endPoint = new URL(URL_MAPPING + "/cancelLikeCount");
+                                    HttpURLConnection myConnection = (HttpURLConnection) endPoint.openConnection();
+                                    myConnection.setRequestMethod("POST");
+
+                                    int simple_review_id = Integer.parseInt(reviewId.getText().toString());
+
+                                    String requestParam = String.format("simple_review_id=%d", simple_review_id);
+                                    myConnection.getOutputStream().write(requestParam.getBytes());
+
+                                    if( myConnection.getResponseCode() == 200 ) {
+                                        Log.d(LOG_TAG, "cancel like count" + myConnection.getResponseCode() + "");
+
+                                        BufferedReader in = new BufferedReader(
+                                                new InputStreamReader(myConnection.getInputStream()));
+                                        StringBuffer buffer = new StringBuffer();
+                                        String temp = null;
+
+                                        while((temp = in.readLine()) != null) {
+                                            buffer.append(temp);
+                                        }
+
+                                        Log.d(LOG_TAG, buffer.toString() + "");
+
+                                        Message count = likeHandler.obtainMessage();
+                                        count.arg1 = Integer.parseInt(buffer.toString());
+                                        likeHandler.sendMessage(count);
+                                    }
+
+                                } catch (Exception e) {
+                                    Log.d(LOG_TAG, "에러 발생");
+                                    e.getMessage();
+                                }
+                            }
+
+
+                            try {
+                                Log.d(LOG_TAG, "1");
+                                URL endPoint = new URL(URL_MAPPING + "/addBadCount");
+                                HttpURLConnection myConnection = (HttpURLConnection) endPoint.openConnection();
+                                myConnection.setRequestMethod("POST");
+                                Log.d(LOG_TAG, "2");
+
+                                int simple_review_id = Integer.parseInt(reviewId.getText().toString());
+
+                                String requestParam = String.format("simple_review_id=%d", simple_review_id);
+                                myConnection.getOutputStream().write(requestParam.getBytes());
+                                Log.d(LOG_TAG, "3");
+
+                                if( myConnection.getResponseCode() == 200 ) {
+                                    Log.d(LOG_TAG, "add bad count" + myConnection.getResponseCode() + "");
+
+                                    BufferedReader in = new BufferedReader(
+                                            new InputStreamReader(myConnection.getInputStream()));
+                                    StringBuffer buffer = new StringBuffer();
+                                    String temp = null;
+
+                                    while((temp = in.readLine()) != null) {
+                                        buffer.append(temp);
+                                    }
+
+                                    Log.d(LOG_TAG, buffer.toString() + "");
+
+                                    //
+                                    Message count = badHandler.obtainMessage();
+                                    count.arg1 = Integer.parseInt(buffer.toString());
+                                    badHandler.sendMessage(count);
+                                }
+
+                            } catch (Exception e) {
+                                Log.d(LOG_TAG, "에러 발생" + e.getMessage());
+                                e.getMessage();
+                            }
+
+                        } else {
+                            Log.d(LOG_TAG, "bad btn cancel");
+
+                            try {
+                                URL endPoint = new URL(URL_MAPPING + "/cancelBadCount");
+                                HttpURLConnection myConnection = (HttpURLConnection) endPoint.openConnection();
+                                myConnection.setRequestMethod("POST");
+
+                                int simple_review_id = Integer.parseInt(reviewId.getText().toString());
+
+                                String requestParam = String.format("simple_review_id=%d", simple_review_id);
+                                myConnection.getOutputStream().write(requestParam.getBytes());
+
+                                if( myConnection.getResponseCode() == 200 ) {
+                                    Log.d(LOG_TAG, "cancel bad count" + myConnection.getResponseCode() + "");
+
+                                    BufferedReader in = new BufferedReader(
+                                            new InputStreamReader(myConnection.getInputStream()));
+                                    StringBuffer buffer = new StringBuffer();
+                                    String temp = null;
+
+                                    while((temp = in.readLine()) != null) {
+                                        buffer.append(temp);
+                                    }
+
+                                    Log.d(LOG_TAG, buffer.toString() + "");
+
+                                    Message count = badHandler.obtainMessage();
+                                    count.arg1 = Integer.parseInt(buffer.toString());
+                                    badHandler.sendMessage(count);
+                                }
+
+                            } catch (Exception e) {
+                                Log.d(LOG_TAG, "에러 발생");
+                                e.getMessage();
+                            }
+                        }
+
+                    }
+                });
             }
         });
 
--- app/src/main/java/com/example/tje/food/SimpleReviewAdapter.java
+++ app/src/main/java/com/example/tje/food/SimpleReviewAdapter.java
@@ -12,16 +12,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.tje.food.Model.ReviewListView;
+import com.squareup.picasso.Picasso;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 import java.util.UUID;
 
 public class SimpleReviewAdapter extends RecyclerView.Adapter<SimpleReviewHolder> {
 
     private static final String LOG_TAG = "simplereviewadapter";
+    private static final String URL = "http://192.168.0.3:8080/Final/resources/upload/simpleReview/";
+    private static final String M_IMAGE_URL = "http://192.168.0.3:8080/Final/resources/upload/memberImage/";
 
     List<ReviewListView> data;
-    Bitmap img;
 
     public void setData(List<ReviewListView> data) {
         this.data = data;
@@ -35,7 +40,7 @@ public class SimpleReviewAdapter extends RecyclerView.Adapter<SimpleReviewHolder
 
     @Override
     public void onBindViewHolder(@NonNull final SimpleReviewHolder simpleReviewHolder, int i) {
-        ReviewListView contact = data.get(i);
+        final ReviewListView contact = data.get(i);
 
         simpleReviewHolder.nicknameTv.setText(contact.getMember_nickname());
         simpleReviewHolder.like_count.setText(Integer.toString(contact.getSimple_review_like_count()));
@@ -46,22 +51,54 @@ public class SimpleReviewAdapter extends RecyclerView.Adapter<SimpleReviewHolder
         simpleReviewHolder.total_score_rating.setRating(contact.getTotal_score());
         simpleReviewHolder.review_id.setText(contact.getSimple_review_id() + "");
 
-        /*
-        new AsyncTask<String, Integer, Bitmap>() {
 
-            @Override
-            protected Bitmap doInBackground(String... strings) {
-                img = BitmapFactory.decodeFile("http://static.inven.co.kr/image_2011/site_image/lol/dataninfo/icon/skinfull/leblanc_splash_0.jpg?v=20181221a");
+        if(contact.getFile_name() != null) {
+            Picasso.get().load(URL + contact.getFile_name()).into(simpleReviewHolder.list_image);
+            simpleReviewHolder.list_image.setVisibility(View.VISIBLE);
+        }
 
-                return img;
-            }
+        if(contact.getMember_photo() != null) {
+            Picasso.get().load(M_IMAGE_URL + contact.getMember_photo()).into(simpleReviewHolder.member_photo_btn);
+        } else {
+            Picasso.get().load(M_IMAGE_URL + "default.png").into(simpleReviewHolder.member_photo_btn);
+        }
 
-            @Override
-            protected void onPostExecute(Bitmap bitmap) {
-                simpleReviewHolder.member_photo_btn.setImageBitmap(img);
-            }
-        }.execute();
-        */
+//        new AsyncTask<String, Integer, Bitmap>() {
+//
+//            Bitmap bitmap;
+//
+//            @Override
+//            protected Bitmap doInBackground(String... strings) {
+//                try {
+//
+//                    if (contact.getFile_name() != null) {
+//                        URL url = new URL(M_IMAGE_URL + contact.getMember_photo());
+//                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//                        conn.connect();
+//
+//                        bitmap = BitmapFactory.decodeStream(conn.getInputStream());
+//                    } else {
+//                        URL url = new URL(M_IMAGE_URL + "default.png");
+//                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//                        conn.connect();
+//
+//                        bitmap = BitmapFactory.decodeStream(conn.getInputStream());
+//                    }
+//
+//
+//                } catch (Exception e) {
+//                    Log.d(LOG_TAG, e.getMessage());//
+//                }
+//
+//                return bitmap;
+//            }
+//
+//            @Override
+//            protected void onPostExecute(Bitmap bitmap) {
+//                simpleReviewHolder.member_photo_btn.setImageBitmap(bitmap);
+//            }
+//        }.execute();
+
 
     }
 
